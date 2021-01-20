2557e0a98e7501578896bcf04f4f17809f4058e5
Mikhail <kozlovma@pik.ru>
2021-01-20 03:35:22 +0300

    dz2.1,2.2,2.3,2.5 finish

---
+++ dz2/dz2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz2._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAvgTemp();
+        }
+        public static float GetAvgTemp()
+        {
+            float MaxTemp = UserInput.GetNumFloat("\nУкажите максимальную температуру за сутки [MaxTemp]:");
+            float MinTemp = UserInput.GetNumFloat("\nУкажите минимальную температуру за сутки [MinTemp]:");
+
+            float AvgTemp = (MaxTemp + MinTemp) / 2;
+            Console.WriteLine($"\nСреднесуточная температура составляет:\n{AvgTemp}");
+            Console.ReadKey();
+            return AvgTemp;
+        }
+    }
+
+}
---
+++ dz2/dz2.1/UserInput.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz2._1
+{
+    public class UserInput
+    {
+        public static float GetNumFloat(string userRequest)
+        {
+            float value;
+            while (true)
+            {
+
+                Console.WriteLine(userRequest);
+                try
+                {
+                    value = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    errorMesseg("Введено некорректное значение. Повторите ввод указав десятичное число\n>>");                  
+                }
+            }
+            return value;
+
+
+        }
+        public static int GetNumInt(string userRequest)
+        {
+            int value;
+            while (true)
+            {
+
+                Console.WriteLine(userRequest);
+                try
+                {
+                    value = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    errorMesseg("Введено некорректное значение. Повторите ввод указав целое число\n>>");
+                }
+            }
+            return value;
+        }
+        public static void errorMesseg(string errorMesseg)
+        {
+            var defColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorMesseg);
+            Console.ForegroundColor = defColor;
+        }
+    }
+}
