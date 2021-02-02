acbb538f02e59e9f57d54f8650c50c3b4780921f
Mikhail <kozlovma@pik.ru>
2021-02-02 23:55:56 +0300

    dz4.4 Добавлены 2 рекурсивных, 1 циклический метод.

--- dz4/dz4.4/Program.cs
+++ dz4/dz4.4/Program.cs
@@ -9,52 +9,93 @@ namespace dz4._4
 {
     class Program
     {
-        public static int i;
-        public static int fibonachiNum;
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                int fibonachiNum = UserInput.GetNumInt("Введите порядковый номер числа Фибоначи: ");
+                if (fibonachiNum < 0) fibonachiNum *= -1;
+
+                //Циклический
+                Console.WriteLine("Алгоритм2:Число Фибоначи №{1} = {0}", getFibonachi2(fibonachiNum), fibonachiNum);                
+                //Рекурсивный
+                Console.WriteLine("Алгоритм3:Число Фибоначи №{1} = {0}", getFibonachi3(fibonachiNum).Item2, fibonachiNum);
+                //Рекурсивный
+                Console.WriteLine("Алгоритм4:Число Фибоначи №{1} = {0}", getFibonachi4((ulong)fibonachiNum), fibonachiNum);
+
+
+                //Гибридный?)
+                
+                i = 0;
+                ulong fibonachi1 = 0;
+                ulong fibonachi2 = 1;
+                ulong fibonachi3 = 1;
+                Console.WriteLine("Алгоритм?:Число Фибоначи №{1} = {0}", getFibonachi1(ref fibonachi3, ref fibonachi1, ref fibonachi2, fibonachiNum), fibonachiNum);
+                
+            }
+       
+        }
+
+        public static int i = 0;
+        public static ulong getFibonachi1(ref ulong fibonachi3, ref ulong fibonachi1, ref ulong fibonachi2, int fibonachiNum)
+        {
+            if (fibonachiNum == 0) return 0;
+
+
+            while (i < fibonachiNum)
+            {
+                if (fibonachi3 >= 18_446_744_073_709_551_615) { Console.WriteLine("Ошибка!Превышен допустимый размер числа."); return 0; }
+                fibonachi3 = fibonachi1 + fibonachi2;
+                fibonachi1 = fibonachi2;
+                fibonachi2 = fibonachi3;
+                i++;
+                getFibonachi1(ref fibonachi3, ref fibonachi1, ref fibonachi2, fibonachiNum);
+            }
+
+            return fibonachi1;
+        }
+        public static ulong getFibonachi2(int fibonachiNum)
+        {
+            ulong fibonachi3 = 1;
             ulong fibonachi1 = 0;
             ulong fibonachi2 = 1;
-            ulong fibonachi3 = 1;
-            fibonachiNum = UserInput.GetNumInt("Введите порядковый номер числа Фибоначи: ");
 
-            if (fibonachiNum==0)
+            for (int j = 0; j < fibonachiNum; j++)
             {
-                Console.WriteLine("Число Фибоначи №0 = 0");
-                Console.ReadKey();
-                return;
-            }
 
-            i = 1;
-
-
-            Console.WriteLine("Число Фибоначи №{1} = {0}",  getFibonachi(ref fibonachi3, ref fibonachi1, ref fibonachi2), i-1);
-
-            Console.ReadKey();
-        }
-        
-        public static ulong getFibonachi(ref ulong fibonachi3, ref ulong fibonachi1, ref ulong fibonachi2)
-        {
-            
-            fibonachi3 = fibonachi1 + fibonachi2;
-            fibonachi1 = fibonachi2;
-            fibonachi2 = fibonachi3;
-            
-            Console.WriteLine("{0} {1}",i++ , fibonachi3);
-
-            while (i <= fibonachiNum)
-            {
-                
                 if (fibonachi3 >= 18_446_744_073_709_551_615)
                     break;
-                fibonachi3 = getFibonachi(ref fibonachi3, ref fibonachi1, ref fibonachi2);
-                
+
+                fibonachi3 = fibonachi1 + fibonachi2;
+                fibonachi1 = fibonachi2;
+                fibonachi2 = fibonachi3;
+
             }
 
-            return fibonachi3;
+            return fibonachi1;
 
+        }
+        public static ulong getFibonachi4(ulong fibonachiNum)
+        {
+            if (fibonachiNum == 0 || fibonachiNum == 1)
+            {
+                return (ulong)fibonachiNum;
             }
-            
-            
+            else
+            {
+                return getFibonachi4(fibonachiNum - 1) + getFibonachi4(fibonachiNum - 2);
+            }
+
+        }
+        public static (ulong, ulong, ulong, int) getFibonachi3(int fibonachiNum, int iterator = 0, ulong f3=2, ulong f1=0, ulong f2=1)
+        {
+            if (iterator++ < fibonachiNum)
+            {
+               return getFibonachi3(fibonachiNum, iterator, (f3 = f1 + f2), (f1 = f2), (f2 = f3));
+            }
+            return (f3, f1, f2, iterator);
+
+        }
     }
 }
