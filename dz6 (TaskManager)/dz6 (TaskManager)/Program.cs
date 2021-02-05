59ffafcf8704c17ce12295d628ac2a46b7aaac25
Mikhail <kozlovma@pik.ru>
2021-02-05 23:02:39 +0300

    dz6 start

---
+++ dz6 (TaskManager)/dz6 (TaskManager)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace dz6__TaskManager_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                var processes = Process.GetProcesses();
+
+                for (int i = 0; i < processes.Length; i++)
+                {
+                    Console.CursorLeft = 0;
+                    Console.Write(processes[i].Id);
+                    Console.CursorLeft = 10;
+                    Console.Write(processes[i].ProcessName);
+                    Console.WriteLine();
+                }
+                Console.Write("Prompt ID to kill:>");
+
+                if (int.TryParse(Console.ReadLine(), out int idToKill))
+                    Process.GetProcessById(idToKill).Kill();
+            }
+            Console.ReadKey();
+        }
+    }
+}
