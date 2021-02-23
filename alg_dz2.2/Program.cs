f8ed067c319a774e53c4e2e4c5265e5add85b4ab
Mikhail Kozlov <dgao.da@gmail.com>
2021-02-24 01:38:57 +0300

    alg_dz2 start

---
+++ alg_dz2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alg_dz2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
