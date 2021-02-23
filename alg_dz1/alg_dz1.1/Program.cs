116ed69ef388857e2b3211f7428d27cf63b69a1f
Mikhail Kozlov <dgao.da@gmail.com>
2021-02-24 01:32:05 +0300

    alg_dz1 finish

---
+++ alg_dz1/alg_dz1.1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace alg_dz1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+           Console.WriteLine(TestPrimeOrNot(GetNumInt("Напишите число, чтобы проверить простое ли оно:")));
+
+        }
+        public static string TestPrimeOrNot(int n)
+        {
+            int d = 0;
+            int i = 2;
+
+            while (i < n)
+            {
+
+                if (n % i == 0)
+                    d++;
+
+                    i++;
+            }
+
+            if (d == 0)
+            {
+                return "Простое";
+            }
+            else
+            {
+                return "Не простое";
+            }
+        }
+        public static int GetNumInt(string userRequest)
+        {
+           
+            while (true)
+            {
+
+                Console.WriteLine(userRequest);
+
+                   if( int.TryParse(Console.ReadLine(),out int num) )
+                        if (num >= 0)
+                        return num;
+
+                    ErrorMesseg("Введено некорректное значение. Повторите ввод указав целое число от 0 до 2147483647\n>>");
+                
+            }
+            
+        }
+        public static void ErrorMesseg(string errorMesseg)
+        {
+            var defColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorMesseg);
+            Console.ForegroundColor = defColor;
+        }
+    }
+}
+
---
+++ alg_dz1/alg_dz1.3 (fibonachi)Tests/ProgramTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using alg_dz1._3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alg_dz1._3.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void getFibonachiTest()
+        {
+            (int, ulong) [] testArray = 
+                {
+                ( 0, 0 ),
+                ( 1, 1 ),
+                ( 2, 1 ),
+                ( 3, 2 ),
+                ( 4, 3 ),
+                ( 5, 5 ),
+                ( 6, 8 ),
+                ( 7, 13 ),
+                ( 8, 21 ),
+                ( 9, 34 ),
+                ( 10, 55 ),
+
+                };
+            foreach (var num in testArray)
+            {
+              
+
+                ulong f1 = Program.getFibonachi2(num.Item1);
+                ulong f2 = Program.getFibonachi3(num.Item1);
+                ulong f3 = Program.getFibonachi4((ulong)num.Item1);
+
+
+                Program.i = 0;
+                ulong fibonachi1 = 0;
+                ulong fibonachi2 = 1;
+                ulong fibonachi3 = 1;
+                ulong f4 = Program.getFibonachi1(ref fibonachi3, ref fibonachi1, ref fibonachi2, num.Item1);
+
+
+                Assert.IsTrue(f1 == f2 & f1 == f3 & f1 == f4);
+                Assert.AreEqual(f1, num.Item2);
+            }
+
+        }
+
+
+    }
+}
