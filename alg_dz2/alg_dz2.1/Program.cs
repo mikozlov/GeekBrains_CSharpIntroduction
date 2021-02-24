f831216e113c5dd6b55f176590f5ef77073aece3
Mikhail <kozlovma@pik.ru>
2021-02-25 01:05:43 +0300

    in progress

---
+++ alg_dz2/alg_dz2.1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace alg_dz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+    public class TwoWayLinkedList : ILinkedList
+    {
+       public Node FirstNode { get; set; }
+       public Node LastNode { get; set; }
+
+        void ILinkedList.AddNode(int value)
+        {
+            if (FirstNode.Value == null)
+            {
+                FirstNode = new Node();
+                LastNode = FirstNode;
+            }
+
+            var node = LastNode;
+
+            node.Value = value;
+            node.PrevNode = node.NextNode;
+            node.NextNode = null;
+
+
+        }
+
+        void ILinkedList.AddNodeAfter(Node node, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        Node ILinkedList.FindNode(int searchValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ILinkedList.GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILinkedList.RemoveNode(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILinkedList.RemoveNode(Node node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
